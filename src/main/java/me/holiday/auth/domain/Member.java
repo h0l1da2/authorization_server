a24f1cc922573cdf4985785ee353c816120440a6
h0l1da2 <holiday.k1@icloud.com>
2024-12-05 23:19:35 +0900

    feat(member): 멤버 엔티티 추가
    holiday

---
+++ src/main/java/me/holiday/auth/domain/Member.java
@@ -0,0 +1,29 @@
+package me.holiday.auth.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+    private String password;
+
+    private String name;
+    private String phoneNumber;
+
+    private LocalDateTime createdAt;
+
+}
