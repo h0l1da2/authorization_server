3776699443ce447ab4db9fd6bc1cd1e7bed61595
h0l1da2 <holiday.k1@icloud.com>
2024-12-05 20:38:47 +0900

    feat: 베이스 패키지 설정 추가
    holiday

--- src/main/java/me/holiday/auth/AuthApplication.java
+++ src/main/java/me/holiday/auth/AuthApplication.java
@@ -2,8 +2,10 @@ package me.holiday.auth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "me.holiday.auth")
+@ConfigurationPropertiesScan(basePackages = "me.holiday.auth")
 public class AuthApplication {
 
 	public static void main(String[] args) {
