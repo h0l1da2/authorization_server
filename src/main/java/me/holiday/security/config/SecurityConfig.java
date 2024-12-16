f5438db48db8f2b7cd58b7f59e6767999ac5e766
h0l1da2 <holiday.k1@icloud.com>
2024-12-16 11:45:27 +0900

    remove(jwt): jwt 필터 삭제
    holiday

--- src/main/java/me/holiday/security/config/SecurityConfig.java
+++ src/main/java/me/holiday/security/config/SecurityConfig.java
@@ -2,7 +2,6 @@ package me.holiday.security.config;
 
 import lombok.RequiredArgsConstructor;
 import me.holiday.auth.domain.RoleName;
-import me.holiday.security.filter.JwtFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -11,7 +10,6 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.filter.CorsFilter;
 
 @Configuration
@@ -20,7 +18,7 @@ import org.springframework.web.filter.CorsFilter;
 public class SecurityConfig {
 
     private final CorsFilter corsFilter;
-    private final JwtFilter jwtFilter;
+//    private final JwtFilter jwtFilter;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -36,7 +34,7 @@ public class SecurityConfig {
                         HeadersConfigurer.FrameOptionsConfig::sameOrigin
                 ))
                 .addFilterBefore(corsFilter, CorsFilter.class)
-                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
+//                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
     }
 
